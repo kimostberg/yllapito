28079470f9297750b6849dc7f3e4712a862b900d
kimostberg <kimmo@juustovaara.fi>
2023-03-20 16:10:08 +0300

    Start.ps1 & StartGH.ps1 merge

--- Start.ps1
+++ Start.ps1
@@ -1,4 +1,8 @@
-﻿Start-Transcript .\logs\$env:computername-$(Get-Date -f yyyy-MM-dd)-Winutil.log -Append
+﻿###
+# test
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+New-Item -Path "$env:SystemDrive\maintenance\logs" -ItemType Directory 
+Start-Transcript \maintenance\logs\$env:computername-$(Get-Date -f yyyy-MM-dd)-Winutil.log -Append
 
 # Check if winget is installed
 Write-Host "Checking if Winget is Installed..."
@@ -44,7 +48,7 @@ Write-Host "Winget Installed"
 If (!(((gp HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*).DisplayName -Match "CrystalDiskInfo").Length -gt 0)) {
     winget install crystaldiskinfo -e
 }
-Start-Process 'C:\Program Files\CrystalDiskInfo\DiskInfo64.exe'
+Start-Process "$env:SystemDrive\Program Files\CrystalDiskInfo\DiskInfo64.exe"
 # PromptForChoice Args
 $Title = "Is disk OK?"
 $Prompt = "Wait that CrystalDiskInfo shows result. Is disk OK? Enter your choice"
@@ -62,11 +66,17 @@ switch($Choice)
         Write-Host "Creating Restore Point in case something bad happens"
             Enable-ComputerRestore -Drive "$env:SystemDrive"
             Checkpoint-Computer -Description "RestorePoint1" -RestorePointType "MODIFY_SETTINGS"
-        .\tweaks.ps1
-        .\SetServicesToManual.ps1
-        .\Update.ps1
-        .\DiskClean.ps1
-
+        If (!(((gp HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*).DisplayName -Match "Git").Length -gt 0)) {
+            winget install git -e
+            exit
+        }
+        cd $env:SystemDrive\maintenance
+        rm -r -Force $env:SystemDrive\maintenance\yllapito
+        git.exe clone https://github.com/kimostberg/yllapito.git
+        .\yllapito\tweaks.ps1
+        .\yllapito\SetServicesToManual.ps1
+        .\yllapito\Update.ps1
+        .\yllapito\DiskClean.ps1
         }
     1 { Write-Host "No - Exiting"
         exit

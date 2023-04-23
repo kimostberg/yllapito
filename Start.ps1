3f0c94286b44d9d5678b803e4774ed2c3d44cdfa
kimostberg <kimmo@juustovaara.fi>
2023-04-23 16:42:28 +0300

    Revert back to old

--- Start.ps1
+++ Start.ps1
@@ -4,7 +4,46 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 New-Item -Path "$env:SystemDrive\maintenance\logs" -ItemType Directory 
 Start-Transcript \maintenance\logs\$env:computername-$(Get-Date -f yyyy-MM-dd)-Yllapito.log -Append
 
-Start-Process powershell.exe -Verb RunAs -ArgumentList "-command irm https://raw.githubusercontent.com/kimostberg/yllapito/main/Winget.ps1 | iex | Out-Host" -WindowStyle Normal -ErrorAction Stop
+# Check if winget is installed
+Write-Host "Checking if Winget is Installed..."
+if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe) {
+    #Checks if winget executable exists and if the Windows Version is 1809 or higher
+    Write-Host "Winget Already Installed"
+}
+else {
+    #Gets the computer's information
+    $ComputerInfo = Get-ComputerInfo
+
+    #Gets the Windows Edition
+    $OSName = if ($ComputerInfo.OSName) {
+$ComputerInfo.OSName
+    }else {
+$ComputerInfo.WindowsProductName
+    }
+
+    if (((($OSName.IndexOf("LTSC")) -ne -1) -or ($OSName.IndexOf("Server") -ne -1)) -and (($ComputerInfo.WindowsVersion) -ge "1809")) {
+
+Write-Host "Running Alternative Installer for LTSC/Server Editions"
+
+# Switching to winget-install from PSGallery from asheroto
+# Source: https://github.com/asheroto/winget-installer
+
+Start-Process powershell.exe -Verb RunAs -ArgumentList "-command irm https://raw.githubusercontent.com/ChrisTitusTech/winutil/$BranchToUse/winget.ps1 | iex | Out-Host" -WindowStyle Normal
+
+    }
+    elseif (((Get-ComputerInfo).WindowsVersion) -lt "1809") {
+#Checks if Windows Version is too old for winget
+Write-Host "Winget is not supported on this version of Windows (Pre-1809)"
+    }
+    else {
+#Installing Winget from the Microsoft Store
+Write-Host "Winget not found, installing it now."
+Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
+$nid = (Get-Process AppInstaller).Id
+Wait-Process -Id $nid
+Write-Host "Winget Installed"
+    }
+} 
 
 Write-Host "Checking if CrystalDiskInfo is Installed..."
 If (!(((gp HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*).DisplayName -Match "CrystalDiskInfo").Length -gt 0)) {

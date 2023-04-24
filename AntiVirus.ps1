351d3034a223599bca7ff192405b77d3d5fac4b7
Kimmo Juustovaara <kimmo@LAN.OSTBERG.TK>
2023-04-24 20:46:57 +0300

    Update AntiVirus.ps1

--- AntiVirus.ps1
+++ AntiVirus.ps1
@@ -2,20 +2,21 @@
 $defenderStatus = (Get-Service windefend).Status
 if ($defenderStatus -eq "Running") {
     # Update Microsoft Defender signatures
-    Update-MpSignature
-    # Run a full antivirus scan
-    Start-MpScan -ScanType FullScan
+    Update-MpSignature -Verbose
+    # Run a quick antivirus scan
+    Start-MpScan -ScanType QuickScan -WhatIf
 } else {
     # Microsoft Defender is not running
-    Write-Host "Microsoft Defender is not running."
-}
-
-# Check if sophosinterceptxcli.exe exists in the default installation path
-if (Test-Path "C:\Program Files\Sophos\Endpoint Defense\sophosinterceptxcli.exe") {
-  # Run a system scan with no user interface and verbose output
-  & "C:\Program Files\Sophos\Endpoint Defense\sophosinterceptxcli.exe" scan --noui --verbose --system
-}
-else {
-  # Write an error message
-  Write-Host "sophosinterceptxcli.exe not found"
+    Write-Warning "Microsoft Defender is not running."
+    # Check if sophosinterceptxcli.exe exists in the default installation path
+    $sophosPath = "C:\Program Files\Sophos\Endpoint Defense\sophosinterceptxcli.exe"
+    if (Test-Path $sophosPath) {
+      # Run a system scan with no user interface and verbose output
+      & $sophosPath scan --noui --system
+    }
+    else {
+      # Write an error message and suggest a third option
+      Write-Error "sophosinterceptxcli.exe not found"
+      Write-Host "You may want to try another antivirus software, such as McAfee or Norton."
+    }
 }

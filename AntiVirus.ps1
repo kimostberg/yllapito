61a1cc99c96d4df624de3688e75be4dd9cc22ed3
Kimmo Juustovaara <kimmo@LAN.OSTBERG.TK>
2023-04-25 12:32:12 +0300

    Update AntiVirus.ps1

--- AntiVirus.ps1
+++ AntiVirus.ps1
@@ -7,7 +7,7 @@ if ($defenderStatus -eq "Running") {
     Start-MpScan -ScanType QuickScan -WhatIf
 } else {
     # Microsoft Defender is not running
-    Write-Warning "Microsoft Defender is not running."
+    Write-Host "Microsoft Defender is not running."
     # Check if sophosinterceptxcli.exe exists in the default installation path
     $sophosPath = "C:\Program Files\Sophos\Endpoint Defense\sophosinterceptxcli.exe"
     if (Test-Path $sophosPath) {
@@ -16,7 +16,17 @@ if ($defenderStatus -eq "Running") {
     }
     else {
       # Write an error message and suggest a third option
-      Write-Error "Sophos not found"
-      Write-Host "You may want to try another antivirus software, such as McAfee or Norton."
+      Write-Host "Sophos not found"
+      # Set the download URL and file path
+      Write-Host "Downloading Norton Power Eraser"
+      $DownloadUrl = "https://liveupdate.symantecliveupdate.com/upgrade/NPE/Production/NPE.exe"
+      $FilePath = Join-Path $MaintenancePath "NPE.exe"
+
+      # Download Norton Power Eraser
+      Invoke-WebRequest -Uri $DownloadUrl -OutFile $FilePath
+
+      # Run Norton Power Eraser
+      Write-Host "Running Norton Power Eraser"
+      Start-Process -FilePath $FilePath
     }
 }

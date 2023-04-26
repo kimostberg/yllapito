82e8659d13a108e10539797ac074d234f905a4ef
Kimmo Juustovaara <kimmo@LAN.OSTBERG.TK>
2023-04-26 13:07:57 +0300

    Update Start.ps1
    Added send log files by email

--- Start.ps1
+++ Start.ps1
@@ -124,3 +124,23 @@ foreach ($script in $scripts) {
         & $scriptPath
     }
 }
+$From = "sender@kimostberg.ti"
+$To = "receiver@kimostberg.ti"
+$Cc = "someone.else@kimostberg.ti"
+$Subject = "$env:computername Log Files"
+$Body = "Please find the attached log files."
+$SMTPServer = "smtp.gmail.com"
+$SMTPPort = 587
+$LogDirectory = "$env:SystemDrive\maintenance\logs"
+
+$Attachments = Get-ChildItem $LogDirectory -Filter *.log | Select-Object -ExpandProperty FullName
+
+$Credential = Get-Credential -Message "Please enter your email credentials"
+
+Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -Attachments $Attachments -UseSsl -Credential $Credential
+
+if ($?) {
+    Remove-Item $LogDirectory\*.log
+} else {
+    Write-Error "An error occurred while sending the email"
+}

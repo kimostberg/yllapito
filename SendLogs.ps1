ae5c3aa2bd81ea1870777d5a80de6156bb38a928
Kimmo Juustovaara <kimmo@LAN.OSTBERG.TK>
2023-04-26 13:45:38 +0300

    Update SendLogs.ps1

--- SendLogs.ps1
+++ SendLogs.ps1
@@ -1,13 +1,13 @@
-$From = "sender@kimostberg.ti"
-$To = "receiver@kimostberg.ti"
-$Cc = "someone.else@kimostberg.ti"
+$From = "senders@email.address"
+$To = "receivers@email.address"
+$Cc = "someone.elses@email.address"
 $Subject = "$env:computername Log Files"
 $Body = "Please find the attached log files."
-$SMTPServer = "smtp.gmail.com"
+$SMTPServer = "smtp.server.address"
 $SMTPPort = 587
 $LogDirectory = "$env:SystemDrive\maintenance\logs"
 
-$Attachments = Get-ChildItem $LogDirectory -Filter *.log | Select-Object -ExpandProperty FullName
+$Attachments = Get-ChildItem $LogDirectory | Select-Object -ExpandProperty FullName
 
 $SecurePassword = Read-Host -Prompt "Please enter your email password" -AsSecureString
 $Credential = New-Object System.Management.Automation.PSCredential $From, $SecurePassword
@@ -15,7 +15,7 @@ $Credential = New-Object System.Management.Automation.PSCredential $From, $Secur
 Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -Attachments $Attachments -UseSsl -Credential $Credential
 
 if ($?) {
-    Remove-Item $LogDirectory\*.log
+    Remove-Item $LogDirectory\*
 } else {
     Write-Error "An error occurred while sending the email"
 }

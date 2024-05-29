b4c1d4d904e4030c6f8e393a50e5fcc6f58c19a8
KimOstberg <mail@kimostberg.it>
2024-05-29 11:26:11 +0300

    Total rewrite
    RustDesk official ps1 script with few mods

--- RustDesk.ps1
+++ RustDesk.ps1
@@ -1,16 +1,115 @@
-# Download latest dotnet/codeformatter release from github
+$ErrorActionPreference= 'silentlycontinue'
 
-$repo = "rustdesk/rustdesk"
+# Assign the value random password to the password variable
+$rustdesk_pw=(-join ((65..90) + (97..122) + (48..57) | Get-Random -Count 16 | % {[char]$_}))
 
-$releases = "https://api.github.com/repos/$repo/releases"
+# Get your config string from your Web portal and Fill Below
+$rustdesk_cfg="Qfi0zayZXRLd3VJR2aYdFN14EUUZTcOh0UDNza1EzVFdHaUZ0YWljbSlmWNNHUiojI5V2aiwiIiojIpBXYiwiIiojI5FGblJnIsIycu9Wa0VHbvNnLqR3aus2clRGdzVnciojI0N3boJye"
 
-Write-Host Determining latest release
-$tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+################################### Please Do Not Edit Below This Line #########################################
 
-$file = "rustdesk-$tag-x86_64.exe"
-$download = "https://github.com/$repo/releases/download/$tag/$file"
-#$name = $file.Split(".")[0]
-$downloaded = "c:\maintenance\rustdesk-host=rustdesk.ktj.solutions,key=PsMZiRn9VcFThwEW15k3CSHNq6TPN54WXkdIWwKEvrk=.exe"
+# Run as administrator and stays in the current directory
+if (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000)
+    {
+        Start-Process PowerShell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+        Exit;
+    }
+}
 
-Write-Host Dowloading latest release
-Invoke-WebRequest $download -Out $downloaded
+# This function will return the latest version and download link as an object
+function getLatest()
+{
+    $Page = Invoke-WebRequest -Uri 'https://github.com/rustdesk/rustdesk/releases/latest' -UseBasicParsing
+    $HTML = New-Object -Com "HTMLFile"
+    try
+    {
+        $HTML.IHTMLDocument2_write($Page.Content)
+    }
+    catch
+    {
+        $src = [System.Text.Encoding]::Unicode.GetBytes($Page.Content)
+        $HTML.write($src)
+    }
+
+    # Current example link: https://github.com/rustdesk/rustdesk/releases/download/1.2.3/rustdesk-1.2.3-x86_64.exe
+    $Downloadlink = ($HTML.Links | Where {$_.href -match '(.)+\/rustdesk\/rustdesk\/releases\/download\/\d{1}.\d{1,2}.\d{1,2}(.{0,3})\/rustdesk(.)+x86_64.exe'} | select -first 1).href
+
+    # bugfix - sometimes you need to replace "about:"
+    $Downloadlink = $Downloadlink.Replace('about:', 'https://github.com')
+
+    $Version = "unknown"
+    if ($Downloadlink -match './rustdesk/rustdesk/releases/download/(?<content>.*)/rustdesk-(.)+x86_64.exe')
+    {
+        $Version = $matches['content']
+    }
+
+    if ($Version -eq "unknown" -or $Downloadlink -eq "")
+    {
+        Write-Output "ERROR: Version or download link not found."
+        Exit
+    }
+
+    # Create object to return
+    $params += @{Version = $Version}
+    $params += @{Downloadlink = $Downloadlink}
+    $Result = New-Object PSObject -Property $params
+
+    return($Result)
+}
+
+$RustDeskOnGitHub = getLatest
+
+
+$rdver = ((Get-ItemProperty  "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\RustDesk\").Version)
+
+if ($rdver -eq $RustDeskOnGitHub.Version)
+{
+    Write-Output "RustDesk $rdver is the newest version."
+    Exit
+}
+
+if (!(Test-Path C:\Temp))
+{
+    New-Item -ItemType Directory -Force -Path C:\Temp > null
+}
+
+cd C:\Temp
+
+Invoke-WebRequest $RustDeskOnGitHub.Downloadlink -Outfile "rustdesk.exe"
+Start-Process .\rustdesk.exe --silent-install
+Start-Sleep -seconds 20
+
+$ServiceName = 'Rustdesk'
+$arrService = Get-Service -Name $ServiceName -ErrorAction SilentlyContinue
+
+if ($arrService -eq $null)
+{
+    Write-Output "Installing service"
+    cd $env:ProgramFiles\RustDesk
+    Start-Process .\rustdesk.exe --install-service
+    Start-Sleep -seconds 20
+}
+
+while ($arrService.Status -ne 'Running')
+{
+    Start-Service $ServiceName
+    Start-Sleep -seconds 5
+    $arrService.Refresh()
+}
+
+cd $env:ProgramFiles\RustDesk\
+.\rustdesk.exe --get-id | Write-Output -OutVariable rustdesk_id
+
+.\rustdesk.exe --config $rustdesk_cfg
+
+.\rustdesk.exe --password $rustdesk_pw
+
+Write-Output "..............................................." | Out-File -FilePath $home\Desktop\rustdesk.txt -Append
+# Show the value of the ID Variable
+Write-Output "RustDesk ID: $rustdesk_id" | Out-File -FilePath $home\Desktop\rustdesk.txt -Append
+
+# Show the value of the Password Variable
+Write-Output "Password: $rustdesk_pw" | Out-File -FilePath $home\Desktop\rustdesk.txt -Append
+Write-Output "..............................................." | Out-File -FilePath $home\Desktop\rustdesk.txt -Append

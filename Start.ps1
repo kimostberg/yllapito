ae7ecfdb09cc5c6964df2c3e0d5942e2275cf264
Kim Ostberg <mail@kimostberg.it>
2025-06-14 10:12:28 +0300

    revert back to cloning repo

--- Start.ps1
+++ Start.ps1
@@ -125,8 +125,6 @@ if (Test-Path $yllapitoPath) {
     git config --global --add safe.directory $yllapitoPath
 }
 
-# Load scripts dynamically from the GitHub repository
-$repoUrl = "https://raw.githubusercontent.com/kimostberg/yllapito/main/"
 $scripts = @(
     "Update.ps1",
     "AntiVirus.ps1",
@@ -137,13 +135,9 @@ $scripts = @(
 )
 
 foreach ($script in $scripts) {
-    $scriptUrl = "$repoUrl$script"
-    try {
-        Write-Host "Downloading and executing $script from GitHub..."
-        $scriptContent = Invoke-WebRequest -Uri $scriptUrl -UseBasicParsing
-        Invoke-Expression $scriptContent.Content
-    } catch {
-        Write-Warning "Failed to download or execute $script from GitHub. Error: $_"
+    $scriptPath = Join-Path $yllapitoPath $script
+    if (Test-Path $scriptPath) {
+        & $scriptPath
     }
 }
 Stop-Transcript
